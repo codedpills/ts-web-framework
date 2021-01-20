73679fb242cfc0745d5b954f0760344d5a1aed1b
zak <codedpills@gmail.com>
2021-01-20 05:27:54 +0000

    refactor getters into one line

--- src/models/Model.ts
+++ src/models/Model.ts
@@ -1,19 +1,19 @@
 import { AxiosPromise, AxiosResponse } from "axios";
 
 interface ModelAttributes<T> {
-  set(value: T): void;
-  getAll(): T;
-  get<K extends keyof T>(key: K): T[K];
+    set(value: T): void;
+    getAll(): T;
+    get<K extends keyof T>(key: K): T[K];
 }
 
 interface Sync<T> {
-  fetch(id: number): AxiosPromise;
-  save(data: T): AxiosPromise;
+    fetch(id: number): AxiosPromise;
+    save(data: T): AxiosPromise;
 }
 
 interface Events {
-  on(eventName: string, callback: () => void): void;
-  trigger(eventName: string): void;
+    on(eventName: string, callback: () => void): void;
+    trigger(eventName: string): void;
 }
 
 interface HasId {
@@ -21,47 +21,39 @@ interface HasId {
 }
 
 export class Model<T extends HasId> {
-  constructor(
-    private attributes: ModelAttributes<T>,
-    private events: Events,
-    private sync: Sync<T>
-  ) {}
+    constructor(
+        private attributes: ModelAttributes<T>,
+        private events: Events,
+        private sync: Sync<T>
+    ) { }
 
-  get on() {
-    return this.events.on;
-  }
+    on = this.events.on;
+    trigger = this.events.trigger;
+    get = this.attributes.get;
 
-  get trigger() {
-    return this.events.trigger;
-  }
-
-  get get() {
-    return this.attributes.get;
-  }
-
-  set(update: T): void {
-    this.attributes.set(update);
-    this.events.trigger("change");
-  }
-
-  fetch(): void {
-    const id = this.get("id");
-    if (typeof id !== "number") {
-      throw new Error("Cannot fetch a user without an id.");
+    set(update: T): void {
+        this.attributes.set(update);
+        this.events.trigger("change");
     }
-    this.sync.fetch(id).then((response: AxiosResponse): void => {
-      this.set(response.data);
-    });
-  }
 
-  save(): void {
-    this.sync
-      .save(this.attributes.getAll())
-      .then((response: AxiosResponse): void => {
-        this.trigger("save");
-      })
-      .catch((error): void => {
-        this.trigger("error");
-      });
-  }
+    fetch(): void {
+        const id = this.get("id");
+        if (typeof id !== "number") {
+            throw new Error("Cannot fetch a user without an id.");
+        }
+        this.sync.fetch(id).then((response: AxiosResponse): void => {
+            this.set(response.data);
+        });
+    }
+
+    save(): void {
+        this.sync
+            .save(this.attributes.getAll())
+            .then((response: AxiosResponse): void => {
+                this.trigger("save");
+            })
+            .catch((error): void => {
+                this.trigger("error");
+            });
+    }
 }
